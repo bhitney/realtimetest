6cbb93a6d10d6833b074c3850b0335481c6ad8c4
bhitney <bhitney@microsoft.com>
2023-11-05 13:56:11 -0500

    Added metrics for each stock

--- Dockerfile
+++ Dockerfile
@@ -7,21 +7,28 @@ ENV PYTHONDONTWRITEBYTECODE=1
 # Turns off buffering for easier container logging
 ENV PYTHONUNBUFFERED=1
 
-ENV increasechance 0.502
-ENV sigma 0.05
-ENV mu 1
-ENV stockstartprice 100
-ENV stockceiling 1200
-ENV stockfloor 1
+ENV marketcorrectionincreasechance 0.504
 ENV marketcorrectionchance 0.002
-ENV marketcorrectionlength 60
-ENV marketcorrectionmodifier 0.7
-ENV individualcorrectionchance 0.005
-ENV individualcorrectionlength 30
-ENV individualcorrectionmodifier 0.4
-
-ENV eventconnectionstring "Endpoint=sb://<<NAME>>.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=<<KEY>>"
+ENV marketcorrectionlength 30
+ENV marketcorrectionmodifier 0.6
+ENV eventconnectionstring "Endpoint=sb://abc123.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=abc123"
 ENV eventhubname "fwtheventhub"
+
+# stock variables: 
+# start price | min price | max price | mu | sigma | correction chance | correction length | correction modifier
+
+# last five:
+# 0-20 increase chance | 20-40 increase chance | 40-60 increase chance | 60-80 increase chance | 80-100 increase chance
+
+# default values: "600|1|1200|1|0.04|0.004|30|0.4|0.557|0.557|0.557|0.557|0.557"
+ENV WHO_vars "600|1|1200|1|0.04|0.004|30|0.4|0.600|0.557|0.557|0.557|0.490"
+ENV WHAT_vars "600|1|1200|1|0.04|0.004|30|0.4|0.600|0.557|0.557|0.557|0.490"
+ENV IDK_vars "600|1|1200|1|0.04|0.004|30|0.4|0.600|0.557|0.557|0.557|0.490"
+ENV WHY_vars "600|1|1200|1|0.04|0.004|30|0.4|0.600|0.557|0.557|0.557|0.490"
+ENV BCUZ_vars "600|1|1200|1|0.04|0.004|30|0.4|0.600|0.557|0.557|0.557|0.490"
+ENV TMRW_vars "600|1|1200|1|0.04|0.004|30|0.4|0.600|0.557|0.557|0.557|0.490"
+ENV TDY_vars "600|1|1200|1|0.05|0.004|30|0.4|0.600|0.557|0.557|0.557|0.490"
+ENV IDGD_vars "600|1|1200|1|0.05|0.004|30|0.4|0.600|0.557|0.557|0.557|0.490"
 
 # Install pip requirements
 COPY requirements.txt .
--- main.py
+++ main.py
@@ -14,41 +14,86 @@ from azure.eventhub.aio import EventHubProducerClient
 EVENT_HUB_CONNECTION_STR = os.environ['eventconnectionstring']
 EVENT_HUB_NAME = os.environ['eventhubname']
 
-Mu = float(os.environ['mu']) 
-Sigma = float(os.environ['sigma']) 
-IncreaseChance = float(os.environ['increasechance']) 
-StockStartPrice = float(os.environ['stockstartprice']) 
-
+MarketCorrectionIncreaseChance = float(os.environ['marketcorrectionincreasechance']) 
 MarketCorrectionChance = float(os.environ['marketcorrectionchance']) 
 MarketCorrectionLength = float(os.environ['marketcorrectionlength']) 
 MarketCorrectionModifier = float(os.environ['marketcorrectionmodifier']) 
 
-IndividualCorrectionChance = float(os.environ['individualcorrectionchance']) 
-IndividualCorrectionLength = float(os.environ['individualcorrectionlength']) 
-IndividualCorrectionModifier = float(os.environ['individualcorrectionmodifier']) 
+WHO_vars = os.environ['WHO_vars'].split('|')
+WHAT_vars = os.environ['WHAT_vars'].split('|')
+IDK_vars = os.environ['IDK_vars'].split('|')
+WHY_vars = os.environ['WHY_vars'].split('|')
+BCUZ_vars = os.environ['BCUZ_vars'].split('|')
+TMRW_vars = os.environ['TMRW_vars'].split('|')
+TDY_vars = os.environ['TDY_vars'].split('|')
+IDGD_vars = os.environ['IDGD_vars'].split('|')
 
-StockFloor = float(os.environ['stockfloor']) 
-StockCeiling = float(os.environ['stockceiling']) 
+# stock variables: 0 start price | 1 min price | 2 max price | 3 mu | 4 sigma | 
+# 5 correction chance | 6 correction length | 7 correction modifier |
+# 8 0-20 increase chance | 9 20-40 increase chance | 10 40-60 | 11 60-80 | 12 80-100
 
-print("Mu: " + str(Mu))
-print("Sigma: " + str(Sigma))
-print("Increasing Chance: " + str(IncreaseChance))
-print("StockStartPrice: " + str(StockStartPrice))
+class StockVariables:
+    def __init__(self, stockVariables) -> None:
+        self.startPrice = float(stockVariables[0])
+        self.minPrice = float(stockVariables[1])
+        self.maxPrice = float(stockVariables[2])
+        self.currentPrice = float(stockVariables[0])
+        self.mu = float(stockVariables[3])
+        self.sigma = float(stockVariables[4])
+        self.correctionChance = float(stockVariables[5])
+        self.correctionLength = int(stockVariables[6])
+        self.correctionCounter = 0
+        self.correctionModifier = float(stockVariables[7])
+        self.isInCorrection = False
+        self.isCorrectionUpwards = True
+        self.aboveStartingCount = 1
+        self.belowStartingCount = 1
+        self.moveUpCount = 1
+        self.moveDownCount = 1
+        self.increaseChance_0_20 = float(stockVariables[8])
+        self.increaseChance_20_40 = float(stockVariables[9])
+        self.increaseChance_40_60 = float(stockVariables[10])
+        self.increaseChance_60_80 = float(stockVariables[11])
+        self.increaseChance_80_100 = float(stockVariables[12])
+
+    def getPriceRange(self):
+        return self.currentPrice / (self.maxPrice - self.minPrice)
+    
+    def getIncreaseChance(self):
+        r = self.getPriceRange()
+        if (r >= 0.80): return self.increaseChance_80_100
+        if (r >= 0.60): return self.increaseChance_60_80
+        if (r >= 0.40): return self.increaseChance_40_60
+        if (r >= 0.20): return self.increaseChance_20_40
+        return self.increaseChance_0_20
+
 print("EventHub: " + EVENT_HUB_NAME)
 print("EventHubConnString: " + EVENT_HUB_CONNECTION_STR)
 
-# symbol, starting prince, in stock correction, correction length, correction upwards
+print("WHO: ", WHO_vars)
+print("WHAT: ", WHAT_vars)
+print("IDK: ", IDK_vars)
+print("WHY: ", WHY_vars)
+print("BCUZ: ", BCUZ_vars)
+print("TMRW: ", TMRW_vars)
+print("TDY: ", TDY_vars)
+print("IDGD: ", IDGD_vars)
+
 dataTable = [
-     ['WHO', StockStartPrice, False, IndividualCorrectionLength, True] 
-    ,['WHAT', StockStartPrice, False, IndividualCorrectionLength, True] 
-    ,['IDK', StockStartPrice, False, IndividualCorrectionLength, True] 
-    ,['WHY', StockStartPrice, False, IndividualCorrectionLength, True] 
-    ,['BCUZ', StockStartPrice, False, IndividualCorrectionLength, True] 
-    ,['TMRW', StockStartPrice, False, IndividualCorrectionLength, True] 
-    ,['TDY', StockStartPrice, False, IndividualCorrectionLength, True] 
-    ,['IDGD', StockStartPrice, False, IndividualCorrectionLength, True] 
+     ['WHO', StockVariables(WHO_vars)] 
+    ,['WHAT', StockVariables(WHAT_vars)] 
+    ,['IDK', StockVariables(IDK_vars)] 
+    ,['WHY', StockVariables(WHY_vars)] 
+    ,['BCUZ', StockVariables(BCUZ_vars)] 
+    ,['TMRW', StockVariables(TMRW_vars)] 
+    ,['TDY', StockVariables(TDY_vars)] 
+    ,['IDGD', StockVariables(IDGD_vars)] 
     ]
 
+totalInitialMarketCap = 0
+for record in dataTable:
+    totalInitialMarketCap = totalInitialMarketCap + record[1].startPrice
+    
 async def run():
     # Create a producer client to send messages to the event hub.
     # Specify a connection string to your event hubs namespace and
@@ -63,6 +108,8 @@ async def run():
         currentCorrectionCount = MarketCorrectionLength
         correctionUpward = False
         count = 0
+        aboveMarketCount = 1
+        belowMarketCount = 1
 
         while True:
             
@@ -72,20 +119,19 @@ async def run():
             if isMarketCorrection == False and random.random() < MarketCorrectionChance:
                     isMarketCorrection = True
                     currentCorrectionCount = MarketCorrectionLength
-                    correctionUpward = random.random() < IncreaseChance
+                    correctionUpward = random.random() < MarketCorrectionIncreaseChance
                     print("Market Correction (" + ("UP" if correctionUpward else "DOWN") + ")")
 
             totalMarketCap = 0
 
-            for record in dataTable:    
-                symbol = record[0]
-                price = record[1]
-                stockCorrection = record[2]
-                stockCorrectionLength = record[3]
-                stockCorrectionIncrease = record[4]
+            for record in dataTable:
 
-                priceIncDec = abs(price - (random.normalvariate(Mu, Sigma) * price))
-                priceIncrease = random.random() < IncreaseChance
+                symbol = record[0]
+                stockVariables = record[1]
+                price = stockVariables.currentPrice
+
+                priceIncDec = abs(price - (random.normalvariate(stockVariables.mu, stockVariables.sigma) * price))
+                priceIncrease = random.random() < stockVariables.getIncreaseChance()
  
                 if isMarketCorrection:
                     if currentCorrectionCount <= 0:
@@ -94,37 +140,51 @@ async def run():
                         priceIncrease = correctionUpward # force direction if in correction
                         priceIncDec = priceIncDec * MarketCorrectionModifier # make corrections more gradual
                 else:
-                    if stockCorrection == False and random.random() < IndividualCorrectionChance:
-                        stockCorrection = True
-                        stockCorrectionLength = IndividualCorrectionLength
-                        stockCorrectionIncrease = random.random() < IncreaseChance
-                        print(symbol + " Correction (" + ("UP" if stockCorrectionIncrease else "DOWN") + ")")
+                    if stockVariables.isInCorrection == False and random.random() < stockVariables.correctionChance:
+                        stockVariables.isInCorrection = True
+                        stockVariables.correctionCounter = stockVariables.correctionLength
+                        stockVariables.isCorrectionUpwards = random.random() < stockVariables.getIncreaseChance()
+                        print(symbol + " Correction (" + ("UP" if stockVariables.isCorrectionUpwards else "DOWN") + ")")
 
-                if stockCorrection:
-                    if stockCorrectionLength <= 0:
-                        stockCorrection = False
+                if stockVariables.isInCorrection:
+                    if stockVariables.correctionCounter <= 0:
+                        stockVariables.isInCorrection = False
                     else:
-                        priceIncrease = stockCorrectionIncrease # force direction if in correction
-                        priceIncDec = priceIncDec * IndividualCorrectionModifier # make corrections more gradual
-                        stockCorrectionLength = stockCorrectionLength - 1
+                        priceIncrease = stockVariables.isCorrectionUpwards # force direction if in correction
+                        priceIncDec = priceIncDec * stockVariables.correctionModifier # make corrections more gradual
+                        stockVariables.correctionCounter -= 1
 
                 if priceIncrease:
                     newPrice = round(price + priceIncDec,2)
-                    newPrice = (newPrice if newPrice < StockCeiling else StockCeiling)
+                    newPrice = (newPrice if newPrice < stockVariables.maxPrice else stockVariables.maxPrice)
+                    stockVariables.moveUpCount += 1
                     #increase
                 else:
                     newPrice = round(price - priceIncDec,2)
-                    newPrice = (newPrice if newPrice > StockFloor else StockFloor)
+                    newPrice = (newPrice if newPrice > stockVariables.minPrice else stockVariables.minPrice)
+                    stockVariables.moveDownCount += 1
                     #decrease
 
-                record[1] = newPrice
-                record[2] = stockCorrection
-                record[3] = stockCorrectionLength
-                record[4] = stockCorrectionIncrease
+                stockVariables.currentPrice = newPrice
+                if (stockVariables.currentPrice > stockVariables.startPrice):
+                    stockVariables.aboveStartingCount += 1
+                else:
+                    stockVariables.belowStartingCount += 1
+                
+                record[1] = stockVariables
                  
                 reading = {'symbol': symbol, 'price': newPrice, 'timestamp': timestamp}
                 s = json.dumps(reading)
-                print(s)
+                if stockVariables.isInCorrection:
+                    if count%60==0:
+                        print(s + " (" + ("UP" if stockVariables.isCorrectionUpwards else "DOWN") + ")")
+                else:
+                    if count%10==0:
+                        print(s + f' (+/-: {stockVariables.aboveStartingCount - stockVariables.belowStartingCount}) ' +
+                            f'(O/U: {stockVariables.aboveStartingCount/stockVariables.belowStartingCount:.2f}) ' + 
+                            f'(U/D: {stockVariables.moveUpCount/stockVariables.moveDownCount:.2f}) ' +
+                            f'(R: {stockVariables.getPriceRange():.2f}) ' +
+                            f'(IC: {stockVariables.getIncreaseChance():.4f})')
 
                 event_data_batch.add(EventData(s)) # add event data to the batch
 
@@ -134,11 +194,18 @@ async def run():
             await producer.send_batch(event_data_batch)
 
             if isMarketCorrection:
-                currentCorrectionCount = currentCorrectionCount - 1
+                currentCorrectionCount -= 1
 
-            count = count + 1
-            print(f'{count} Total Market Cap: {totalMarketCap:.2f} (Avg {totalMarketCap/len(dataTable):.2f})')
-           
+            if (totalMarketCap >= totalInitialMarketCap):
+                aboveMarketCount += 1
+            else:
+                belowMarketCount += 1
+
+            if count%10==0:
+                print(f'{count} | Total Market Cap: {totalMarketCap:.2f} | Avg: {totalMarketCap/len(dataTable):.2f} | O/U: {aboveMarketCount/belowMarketCount:.2f}')
+            
+            count += 1
+                
             time.sleep(1)
 
 asyncio.run(run())    
